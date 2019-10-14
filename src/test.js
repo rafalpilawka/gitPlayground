810809e4c8005e580a16fdb88d116e248215adc2
RAF <raf@MacBook-Pro-RAF.local>
2019-10-14 07:40:11 +0200

    newbranch befroe split

--- src/test.js
+++ src/test.js
@@ -5,6 +5,7 @@ const Test = () => {
         <div>
             <p>Not receiving new lines</p>
             <p>need to make soe stuff</p>
+            <p>Add some stuff for tip</p>
         </div>
     )
 }
